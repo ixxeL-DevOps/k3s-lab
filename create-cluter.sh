9e86a3b2ecbf78725243539cffc074da149fa713
ixxeL2097 <fredspiers@gmail.com>
2023-12-09 14:03:03 +0100

    feat: added coreDNS custom replacement

--- create-cluter.sh
+++ create-cluter.sh
@@ -5,4 +5,6 @@ DIR=$(pwd)/post-install/:/var/lib/rancher/k3s/server/manifests/
 yq eval -i '.volumes[].volume = "'"$DIR"'"' k3s.yaml
 
 k3d cluster delete --config k3s.yaml
-k3d cluster create --config k3s.yaml
+k3d cluster create --wait --config k3s.yaml
+
+sed -i 's/forward \. \/etc\/resolv\.conf/forward \. 192.168.1.200/' post-install/coredns.yaml
